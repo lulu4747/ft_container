d616300f6a700b2f3903155f071d33aa0cb32680
lulu4747 <ludogriph@gmail.com>
2021-08-21 15:31:05 +0200

    member types

--- Vector.hpp
+++ Vector.hpp
@@ -11,12 +11,51 @@ namespace	ft
 	class Vector{
 
 	public:
-		Vector(void);
+
+	//	Member Types :
+
+		typedef T					value_type;
+		typedef	Alloc				allocator_type;
+		typedef	size_t				size_type;
+		typedef	ptrdiff_t			difference_type;
+		typedef	value_type&			reference;
+ 		typedef const value_type&	const_reference;
+
+		typedef typename	Alloc::pointer			pointer;
+		typedef typename	Alloc::const_pointer	const_pointer;
+/*	need to check iterators
+iterator 	LegacyRandomAccessIterator to value_type
+const_iterator 	LegacyRandomAccessIterator to const value_type
+reverse_iterator 	std::reverse_iterator<iterator>
+const_reverse_iterator 	std::reverse_iterator<const_iterator>*/
+
+	//	Member Functions :
+
+		explicit Vector (const allocator_type& alloc = allocator_type())
+		:
+			_alloc(alloc),
+			_size(0),
+			_capacity(0)
+		{}
+
+		explicit Vector (size_type n, const value_type& val = value_type(),
+				const allocator_type& alloc = allocator_type());
+
+		template <class InputIterator>
+		Vector (InputIterator first, InputIterator last,
+				const allocator_type& alloc = allocator_type());
+
+		Vector (const Vector& x);
+
 		~Vector();
 
 	private:
-		/* data */
 
+		allocator_type	_alloc;
+		value_type*		_value;
+
+		size_type		_size;
+		size_type		_capacity;
 	};
 }
 
