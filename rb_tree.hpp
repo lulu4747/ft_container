6aec772d7c87f82705cf85881a90b6693fd9b59a
lulu4747 <ludogriph@gmail.com>
2021-11-26 20:35:25 +0100

    unbalanced and untested tree

--- rb_tree.hpp
+++ rb_tree.hpp
@@ -10,18 +10,20 @@ namespace	ft
 	struct node
 	{
 
-		typedef	struct		node*						element;
+		typedef	struct		node*						pointer;
+		typedef	struct		node&						reference;
 		typedef				T							value_type;
-		typedef				value_type*					pointer;
-		typedef				value_type&					reference;
+		typedef				value_type*					value_pointer;
 
-		element	parent;
-		element	left;
-		element	right;
-		pointer	value;
+		typedef	typename	value_type::first&			key_type;
+
+		pointer			parent;
+		pointer			left;
+		pointer			right;
+		value_pointer	value;
 
 		explicit node(reference src_value = value_type(),
-			element const& src_parent = NULL, element const& src_left = NULL, element const& src_right = NULL)
+			pointer const& src_parent = NULL, pointer const& src_left = NULL, pointer const& src_right = NULL)
 		:
 			parent(src_parent),
 			left(src_left),
@@ -36,7 +38,7 @@ namespace	ft
 
 		virtual ~node();
 
-		node&	operator=(node const & rhs)
+		reference	operator=(node const & rhs)
 		{
 			if (this != &rhs)
 			{
@@ -46,6 +48,11 @@ namespace	ft
 				value = rhs.value;
 			}
 			return *this;
+		}
+
+		key_type	key() const
+		{
+			return value->first;
 		}
 
 		bool	operator==(node const & rhs) const
@@ -71,6 +78,7 @@ namespace	ft
 
 		typedef Compare				key_compare;
 		typedef	key					key_type;
+		typedef	size_t				size_type;
 
 		node_pointer			root;
 		node_pointer			end;
@@ -86,7 +94,7 @@ namespace	ft
 		rb_tree(value_type const& val)
 			:rb_tree()
 		{
-			node_alloc.construct(root, val);
+			node_alloc.construct(root, node_type(val));
 			end = root;
 		}
 
@@ -113,6 +121,11 @@ namespace	ft
 			return root == NULL;
 		}
 
+		size_type	size()
+		{
+			return size(root);
+		}
+
 		void	clear()
 		{
 			if (empty())
@@ -124,21 +137,12 @@ namespace	ft
 			node_alloc.destroy(root);
 		}
 
-		void	clear(node_pointer ptr)
-		{
-			if (ptr->left)
-				clear(ptr->left);
-			if (ptr->right)
-				clear(ptr->right);
-			node_alloc.destroy(ptr);
-		}
-
 		void	copy(rb_tree const& src)
 		{
 			clear();
 			if (src.empty())
 				return ;
-			root = new node_type(src.root);
+			node_alloc.construct(root, *(src.root));
 			if (src->left)
 				root->left = copy(root->left, src->left);
 			else
@@ -148,20 +152,6 @@ namespace	ft
 			else
 				root->right = NULL;
 			return ;
-		}
-
-		node_pointer	copy(node_pointer ptr1, node_pointer ptr2)
-		{
-			ptr1 = new node_type(ptr2);
-			if (ptr2->left)
-				ptr1->left = copy(ptr1->left, ptr2->left);
-			else
-				ptr1->left = NULL;
-			if (ptr2->right)
-				ptr1->right = copy(ptr1->right, ptr2->right);
-			else
-				ptr1->right = NULL;
-			return ptr1;
 		}
 
 		void	insert(const value_type& value)
@@ -184,6 +174,112 @@ namespace	ft
 				new_parent->left = new_node;
 			else
 				new_parent->right = new_node;
+		}
+
+		void	erase(node_pointer ptr)
+		{
+			node_pointer	parent(ptr->parent);
+			node_pointer	child(NULL);
+
+			if (ptr == root)
+			{	_root_erase();	return;}
+			if (ptr == parent->left)
+			{
+				if (size(ptr->left) <= size(ptr->right))
+				{
+					parent->left = ptr->right;
+					child = ptr->left;
+				}
+				else if (size(ptr->right) < size(ptr->left))
+				{
+					parent->left = ptr->left;
+					child = ptr->right;
+				}
+				else
+					parent->left = NULL;
+				parent = parent->left;
+			}
+			else
+			{
+				if (size(ptr->left) <= size(ptr->right))
+				{
+					parent->right = ptr->right;
+					child = ptr->left;
+				}
+				else if (size(ptr->right) < size(ptr->left))
+				{
+					parent->right = ptr->left;
+					child = ptr->right;
+				}
+				else
+					parent->right = NULL;
+				parent = parent->right;
+			}
+			node_alloc.destroy(ptr);
+			if (child)
+				_relocate(child, parent);
+		}
+	
+	private:
+
+		size_type	size(node_pointer ptr)
+		{
+			size_type	size = 0;
+
+			if (ptr->left)
+			{
+				size++;
+				size += size(ptr->left);
+			}
+			if (ptr->right)
+			{
+				size++;
+				size += size(ptr->right);
+			}
+			return size;
+		}
+
+		void	clear(node_pointer ptr)
+		{
+			if (ptr->left)
+				clear(ptr->left);
+			if (ptr->right)
+				clear(ptr->right);
+			node_alloc.destroy(ptr);
+		}
+
+		node_pointer	copy(node_pointer ptr1, node_pointer ptr2)
+		{
+			node_alloc.construct(ptr1, *ptr2);
+			if (ptr2->left)
+				ptr1->left = copy(ptr1->left, ptr2->left);
+			else
+				ptr1->left = NULL;
+			if (ptr2->right)
+				ptr1->right = copy(ptr1->right, ptr2->right);
+			else
+				ptr1->right = NULL;
+			return ptr1;
+		}
+
+		void	_root_erase();
+
+		void	_relocate(node_pointer to_relocate, node_pointer to_compare)
+		{
+			if (key_compare(to_relocate->key(), to_compare->key()))
+			{
+				while (to_compare->left)
+					to_compare = to_compare->left;
+				to_compare->left = to_relocate;
+				to_relocate->parent = to_compare;
+			}
+			else
+			{
+				while (to_compare->right)
+					to_compare = to_compare->right;
+				to_compare->right = to_relocate;
+				to_relocate->parent = to_compare;
+			}
 		}
 
 	};
